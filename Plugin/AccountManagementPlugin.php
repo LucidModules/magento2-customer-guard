a9b8f839f46b8265198b72d4dfe792c0f43db419
Maciej Chadała <maciej.chadala@strix.net>
2020-08-14 00:24:12 +0800

    Changed plugin to an Account Manager which is executed for frontend controller, WebAPI and GraphQL

---
+++ Plugin/AccountManagementPlugin.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LS\CustomerGuard\Plugin;
+
+use LS\CustomerGuard\Model\ConfigInterface;
+use LS\CustomerGuard\Model\RegisterConditionInterface;
+use Magento\Customer\Api\AccountManagementInterface;
+use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Framework\Exception\LocalizedException;
+
+/**
+ * Performs Customer registration constraints validation.
+ */
+class AccountManagementPlugin
+{
+    /**
+     * @var RegisterConditionInterface
+     */
+    private $registerCondition;
+
+    /**
+     * @var ConfigInterface
+     */
+    private $config;
+
+    /**
+     * @param RegisterConditionInterface $registerCondition
+     * @param ConfigInterface $config
+     */
+    public function __construct(
+        RegisterConditionInterface $registerCondition,
+        ConfigInterface $config
+    ) {
+        $this->registerCondition = $registerCondition;
+        $this->config = $config;
+    }
+
+    /**
+     * Apply additional validations.
+     *
+     * @param AccountManagementInterface $subject
+     * @param CustomerInterface $customer
+     * @param null $password
+     * @param string $redirectUrl
+     * @return array
+     * @throws LocalizedException
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function beforeCreateAccount(
+        AccountManagementInterface $subject,
+        CustomerInterface $customer,
+        $password = null,
+        $redirectUrl = ''
+    ): array {
+        $this->throwOnFailingCustomerConstraints($customer);
+
+        return [$customer, $password, $redirectUrl];
+    }
+
+    /**
+     * Apply additional validations.
+     *
+     * @param AccountManagementInterface $subject
+     * @param CustomerInterface $customer
+     * @param $hash
+     * @param string $redirectUrl
+     * @return array
+     * @throws LocalizedException
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function beforeCreateAccountWithPasswordHash(
+        AccountManagementInterface $subject,
+        CustomerInterface $customer,
+        $hash,
+        $redirectUrl = ''
+    ): array {
+        $this->throwOnFailingCustomerConstraints($customer);
+
+        return [$customer, $hash, $redirectUrl];
+    }
+
+    /**
+     * Apply additional validations.
+     *
+     * @param AccountManagementInterface $subject
+     * @param CustomerInterface $customer
+     * @return array
+     * @throws LocalizedException
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function beforeValidate(
+        AccountManagementInterface $subject,
+        CustomerInterface $customer
+    ): array {
+        $this->throwOnFailingCustomerConstraints($customer);
+
+        return [$customer];
+    }
+
+    /**
+     * Throws when customer does not meet configured constraints.
+     *
+     * @param CustomerInterface $customer
+     * @throws LocalizedException
+     */
+    private function throwOnFailingCustomerConstraints(CustomerInterface $customer): void
+    {
+        if (!$this->registerCondition->isAllowed($customer)) {
+            throw new LocalizedException(__($this->config->getRegisterRestrictionMessage()));
+        }
+    }
+}
